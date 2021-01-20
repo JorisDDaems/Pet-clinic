796728b128561b67aa1412d8e12675851c6daace
JorisDDaems <jorisddaems@gmail.com>
2021-01-20 11:49:55 +0100

    Added specialities Map. closes #32

---
+++ pet-clinic-data/src/main/java/be/intecbrussel/petclinic/services/SpecialityService.java
@@ -0,0 +1,8 @@
+package be.intecbrussel.petclinic.services;
+
+import be.intecbrussel.petclinic.model.Speciality;
+
+public interface SpecialityService extends CrudService<Speciality, Long> {
+
+
+}
