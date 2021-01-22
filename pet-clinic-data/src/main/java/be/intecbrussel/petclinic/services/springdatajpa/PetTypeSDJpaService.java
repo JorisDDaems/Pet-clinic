6ec0f80c0aad31f3e226ad9569bf8d459a608556
JorisDDaems <jorisddaems@gmail.com>
2021-01-22 12:19:50 +0100

    Added spring data jpa service for pets. Closes #44

---
+++ pet-clinic-data/src/main/java/be/intecbrussel/petclinic/services/springdatajpa/PetTypeSDJpaService.java
@@ -0,0 +1,33 @@
+package be.intecbrussel.petclinic.services.springdatajpa;
+
+import be.intecbrussel.petclinic.model.PetType;
+import be.intecbrussel.petclinic.services.PetTypeService;
+
+import java.util.Set;
+
+public class PetTypeSDJpaService implements PetTypeService {
+    @Override
+    public Set<PetType> findAll() {
+        return null;
+    }
+
+    @Override
+    public PetType findById(Long aLong) {
+        return null;
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return null;
+    }
+
+    @Override
+    public void delete(PetType object) {
+
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+
+    }
+}
