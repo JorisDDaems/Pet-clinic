adac84b7e03967c80e785258025d49ffee173950
JorisDDaems <jorisddaems@gmail.com>
2021-02-03 18:24:43 +0100

    Display Owner record. Closes #55

--- pet-clinic-web/src/main/java/be/intecbrussel/petclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/be/intecbrussel/petclinic/controllers/OwnerController.java
@@ -3,7 +3,10 @@ package be.intecbrussel.petclinic.controllers;
 import be.intecbrussel.petclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @RequestMapping("/owners")
 @Controller
@@ -28,5 +31,13 @@ public class OwnerController {
 
         return "notImplemented";
     }
+
+    @GetMapping("/{ownerId}")
+    public ModelAndView showOwner(@PathVariable("ownerId") Long ownerId) {
+        ModelAndView mav = new ModelAndView("owners/ownerDetails");
+        mav.addObject(ownerService.findById(ownerId));
+        return mav;
+    }
+
 
 }
--- pet-clinic-web/src/test/java/be/intecbrussel/petclinic/controllers/OwnerControllerTest.java
+++ pet-clinic-web/src/test/java/be/intecbrussel/petclinic/controllers/OwnerControllerTest.java
@@ -3,9 +3,12 @@ package be.intecbrussel.petclinic.controllers;
 import be.intecbrussel.petclinic.model.Owner;
 import be.intecbrussel.petclinic.services.OwnerService;
 import com.sun.xml.txw2.annotation.XmlElement;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatcher;
+import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -19,6 +22,8 @@ import java.util.Set;
 import static org.hamcrest.Matchers.hasSize;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
+import static org.springframework.data.repository.util.ClassUtils.hasProperty;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -49,17 +54,27 @@ class OwnerControllerTest {
     void listOwners() throws Exception {
         when(ownerService.findAll()).thenReturn(owners);
 
-        mockMvc.perform(MockMvcRequestBuilders.get("/owners/index")).andExpect(status().isOk())
+        mockMvc.perform(get("/owners/index")).andExpect(status().isOk())
                 .andExpect(view().name("owners/index")).andExpect(model().attribute("owners", hasSize(2)));
     }
 
     @Test
     void findOwners() throws Exception {
 
-        mockMvc.perform(MockMvcRequestBuilders.get("/owners/find"))
+        mockMvc.perform(get("/owners/find"))
                 .andExpect(status().isOk())
                 .andExpect(view().name("notImplemented"));
 
         verifyZeroInteractions(ownerService);
+    }
+
+    @Test
+    void displayOwner() throws Exception {
+        when(ownerService.findById(anyLong())).thenReturn(Owner.builder().id(1l).build());
+
+        mockMvc.perform(get("/owners/123"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("owners/ownerDetails"))
+                .andExpect(model().attribute("owner", Matchers.hasProperty("id", Matchers.is(1l))));
     }
 }
